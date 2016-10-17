ae6efb5b22c7771ecc003bd4dd0429b9697a11f5
Helen Maherovska <maher.olena96@gmail.com>
2016-10-17 16:13:48 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -14,3 +14,27 @@ $(function() {
     });
 
 });
+
+(function(){
+$(function(){
+    $('#submit').click(function(e){
+        if($('#name').val().match(/[A-ZА-ЯЁ]+/i)&&$('#email').val().match(/^.+@\w+\.\w+$/i)){
+        e.preventDefault();
+        $.ajax({
+              url: "https://formspree.io/maher.olena96@gmail.com", 
+            method: "POST",
+    data: {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        message: $('#message').val(),
+       
+           
+    },
+    dataType: "json"
+        }).done(function(){
+            $('form').html('<p class="contacts">Thank you for contacting me!</p>');
+        }).fail(function(xhr){
+            $('body').append('Sorry, error occured'+ xhr.responseText);
+        });
+}});
+})})();
